77a66ea4fa59848e402d5ebb5044dbb6b22775d9
Cory Evans <cory.evans@mailbox.org>
2021-10-18 13:44:08 +1300

    merge js into 1 file. assignment requirements

--- public/javascript.js
+++ public/javascript.js
@@ -1,3 +1,13 @@
+function closeNav() {
+	document.querySelector('#sidebar').classList.add('-right-64')
+	document.querySelector('#sidebar').classList.remove('right-0')
+}
+
+function openNav() {
+	document.querySelector('#sidebar').classList.remove('-right-64')
+	document.querySelector('#sidebar').classList.add('right-0')
+}
+
 const shopItems = [
 	{
 		name: 'Apple Ipad Air (4th gen)',
--- public/js/sidebar.js
+++
@@ -1,9 +0,0 @@
-function closeNav() {
-	document.querySelector('#sidebar').classList.add('-right-64')
-	document.querySelector('#sidebar').classList.remove('right-0')
-}
-
-function openNav() {
-	document.querySelector('#sidebar').classList.remove('-right-64')
-	document.querySelector('#sidebar').classList.add('right-0')
-}
